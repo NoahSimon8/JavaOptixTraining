7f7ada0be517bca3c466310428a7fcce158010ac
NoahSimon8 <noah.simon9806@gmail.com>
2022-10-02 16:32:03 -0700

    PrintingAndVariables

---
+++ Printing.java
@@ -0,0 +1,10 @@
+package JavaOptixTraining;
+
+public class Printing {
+
+    public  static void main(String[] args) {
+        
+        System.out.println("Java is cool \n Your mom is");
+    }
+    
+}
