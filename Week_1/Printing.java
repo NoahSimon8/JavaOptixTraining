3418bd90ee5ec3cced53dbd8d707e968c86a1acd
NoahSimon8 <noah.simon9806@gmail.com>
2022-10-04 16:15:54 -0700

    Printing

---
+++ Week_1/Printing.java
@@ -0,0 +1,15 @@
+package JavaOptixTraining.Week_1;
+
+
+public class Printing {
+
+    public  static void main(String[] args) {
+        System.out.println("Java is cool \nso is your mom");
+
+        
+    }
+    
+}
+
+
+
